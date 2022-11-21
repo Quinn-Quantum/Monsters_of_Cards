16e7e6cf491ca9e29dc231efcead98444511c077
Quinn-Quantum <79361383+Quinn-Quantum@users.noreply.github.com>
2022-11-21 12:47:23 +0100

    Add: Card Object

--- Monster_of_Cards_Game/Assets/Karte/Card.cs
+++ Monster_of_Cards_Game/Assets/Karte/Card.cs
@@ -1,39 +1,56 @@
 using Godot;
+using Godot.Collections;
+using System.Collections.Generic;
 using System;
+using System.IO;
+using System.Linq;
 
 public class Card : Node2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
 
-    // Called when the node enters the scene tree for the first time.
-    private string[] _cards = { "Geist", "Drache", "Pixi", "Banana" };
+	// Called when the node enters the scene tree for the first time.
+	private Texture[] myTextures;
+	private List<String> myTextureList = new List<String>();
 
-    public override void _Ready()
-    {
-        GD.Randomize();
+	public override void _Ready()
+	{
+		myTextureList.Add("res://Assets/Monsters/Monster_01.png");
+		myTextureList.Add("res://Assets/Monsters/Monster_02.png")
+		myTextureList.Add("res://Assets/Monsters/Monster_03.png")
+		myTextureList.Add("res://Assets/Monsters/Monster_04.png")
+		myTextureList.Add("res://Assets/Monsters/Monster_05.png")
+			
+		//GD.Print(myTextureList);
+		GD.Randomize();
 
-        for (int i = 0; i < 100; i++)
-        {
-            // Pick 100 fruits randomly.
-            GD.Print(GetCard());
-        }
+	
+		var texture = ImageTexture.new()
+		var image = Image.new()
+		image.load(GetCard())
+		texture.create_from_image(image)
+		$Sprite.texture = texture
+		
+
+		//var texture = (Texture)GD.Load("res://Assets/Monsters/Monster_02.png"); // resource is loaded when line is executed
+		GD.Print(myTextureList[0]);
 
 
-    }
+	}
 
-    public string GetCard()
-    {
-        string randomCard = _cards[GD.Randi() % _cards.Length];
-        // Returns "apple", "orange", "pear", or "banana" every time the code runs.
-        // We may get the same fruit multiple times in a row.
-        return randomCard;
-    }
+	public string GetCard()
+	{
+		string randomCard = _cards[GD.Randi() % myTextureList.Length];
+		// Returns "apple", "orange", "pear", or "banana" every time the code runs.
+		// We may get the same fruit multiple times in a row.
+		return randomCard;
+	}
 
-    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-    //  public override void _Process(float delta)
-    //  {
-    //      
-    //  }
+	//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+	//  public override void _Process(float delta)
+	//  {
+	//      
+	//  }
 }
