6fdc2991a197f36be5663e3c69d883c29c3a52ba
Quinn-Quantum <79361383+Quinn-Quantum@users.noreply.github.com>
2022-12-03 23:26:11 +0100

    set position
    funktioniert nur, wenn im Node geändert.

--- Monster_of_Cards_Game/Szene/CardGame.cs
+++ Monster_of_Cards_Game/Szene/CardGame.cs
@@ -13,25 +13,33 @@ public class CardGame : Node2D
 	private List<string> myTextureList = new List<string>();
 	public PackedScene simultaneousScene ;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
 		myTextureList.Add("res://Assets/Karte/Monster_01.tscn");
 		myTextureList.Add("res://Assets/Karte/Monster_02.tscn");
 		myTextureList.Add("res://Assets/Karte/Monster_03.tscn");
 		myTextureList.Add("res://Assets/Karte/Monster_04.tscn");
+		myTextureList.Add("res://Assets/Karte/Card.tscn");
 		
 		
 		GD.Randomize();
 		Random random = new Random();
+		GD.Print(random);
 		
 		for(int i = 0; i <3; i++){
 			int index = random.Next(myTextureList.Count);
-			simultaneousScene = (PackedScene)GD.Load(myTextureList[index]);
-			
-			Node test = simultaneousScene.Instance();
-			
+			GD.Print("Hi: ",index);
+			//simultaneousScene = (PackedScene)GD.Load(myTextureList[index]);
+			simultaneousScene = (PackedScene)GD.Load(myTextureList[4]);
+			var test = simultaneousScene.Instance();
+			var zahl = test.GetPositionInParent();
+			GD.Print("HI: ",zahl);
 			AddChild(test);
+			
+		
+			
+			
 			//test.Translation= Vector2(10*i, 10*i);
 		}
 		
